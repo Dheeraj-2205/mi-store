c5f9177ef96b597b28574abf1d399727093273f1
Dheeraj-2205 <djsays124@gmail.com>
2023-03-25 02:26:29 +0530

    using react-bootstap to making a crousel

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import PreNavbar from "./components/PreNavbar";
 import Navbar from "./component/Navbar.js";
 import { BrowserRouter as Router } from "react-router-dom";
 import "../src/styles/PreNavbar.css";
+import Slider from "./component/Slider.js"
 function App() {
   return (
     <Router>
---
+++ src/component/Slider.js
@@ -0,0 +1,17 @@
+import { Carousel } from 'bootstrap'
+import React from 'react'
+
+const Slider = () => {
+  return (
+    <Carousel>
+        <Carousel.Item>
+
+        </Carousel.Item>
+        <Carousel.Caption>
+            
+        </Carousel.Caption>
+    </Carousel>
+  )
+}
+
+export default Slider
