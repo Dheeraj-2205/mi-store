ebca4818abb333342a59cfb4f7284914cf255f36
Dheeraj-2205 <djsays124@gmail.com>
2023-03-24 15:38:34 +0530

    react file setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import PreNavbar from './components/PreNavbar';
+import "../src/styles/PreNavbar.css"
+function App() {
+  return (
+    <div>
+      <PreNavbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PreNavbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const PreNavbar = () => {
+  return (
+    <div className='preNav'>
+        <div>
+            <a href="">MI INDIA</a> <span></span>
+            <a href="">GET MIT STORE</a> <span></span>
+            <a href="">ONLINE</a> <span></span>
+            <a href="">RETAIL STORE</a> <span></span>
+        </div>
+        <div>
+            <a href="">SIGN IN</a><span></span>
+            <a href="">SIGN UP</a><span></span>
+            <a href="">Cart (0)</a>
+            
+        </div>
+    </div>
+    
+  )
+}
+
+export default PreNavbar
