51578566bf5ee2d55412d13ad894b49229fce864
Dheeraj-2205 <djsays124@gmail.com>
2023-03-24 16:42:24 +0530

    create navbar and adding css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import PreNavbar from './components/PreNavbar';
+import Navbar from "./component/Navbar.js"
 import "../src/styles/PreNavbar.css"
 function App() {
   return (
     <div>
       <PreNavbar/>
+      <Navbar/>
     </div>
   );
 }
---
+++ src/component/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../styles/nav.css"
+const Navbar = () => {
+  return (
+    <div className='nav'>
+        lfkafjkla
+    </div>
+  )
+}
+
+export default Navbar
