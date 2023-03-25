4938deb8882fd6409dc84ed780193f9aa2aef413
Dheeraj-2205 <djsays124@gmail.com>
2023-03-25 13:59:26 +0530

    adding react-bootsrap in carousel

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Navbar from "./component/Navbar.js";
 import { BrowserRouter as Router } from "react-router-dom";
 import "../src/styles/PreNavbar.css";
 import Slider from "./component/Slider.js"
+import banner from "./data/banner.json"
 function App() {
   return (
     <Router>
       <PreNavbar />
       <Navbar />
-      <Slider/>
+      <Slider start = {banner.banner.start}/>
     </Router>
   );
 }
--- src/component/Slider.js
+++ src/component/Slider.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import Carousel from 'react-bootstrap/Carousel';
-
-const Slider = () => {
+import 'bootstrap/dist/css/bootstrap.min.css';
+const Slider = ({start}) => {
   return (
-    <Carousel>
-        <Carousel.Item>
-        <img src="https://i03.appmifile.com/43_operator_in/24/03/2023/7d08887e241c3352200fc2bfbc843f95.jpg?f=webp"
-            alt="" 
-        />
-        </Carousel.Item>
+    <Carousel fade>
+        {
+          start.map((ele,i)=>{
+            
+            return (
+              <Carousel.Item>
+              <>
+                <img
+                className='sliderimage'
+                 src={ele}
+                 alt="" 
+                />
+              </>
+              </Carousel.Item>
+            )
+          })
+        }
+        
     </Carousel>
   )
 }
-
 export default Slider
